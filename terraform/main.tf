70d39a53753e419af501e5e79d0a7a88ba51f946
Arshad <arshadahmed.de@gmail.com>
2024-04-29 17:49:11 +0530

    adding source for module

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,18 @@ variable "table_name" {}
 
 
 module "ec2_instance" {
-  source        = "github.com/arshad789/test/terraform/modules/ec2"
+  source        = "github.com/arshad7789/assignment/terraform/modules/ec2"
   instance_type = var.instance_type
   ami           = var.ami
 }
 
  module "s3_bucket" {
-   source      = "github.com/arshad789/test/terraform/modules/s3"
+   source      = "github.com/arshad7789/assignment/terraform/modules/s3"
    bucket_name = var.bucket_name
  }
 
  module "dynamodb_table" {
-   source     = "github.com/arshad789/test/terraform/modules/dynamodb"
+   source     = "github.com/arshad7789/assignment/terraform/modules/dynamodb"
    table_name = var.table_name
  }
 
